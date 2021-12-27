b846e041a56954b557775264dbe7363aed2966c8
Pouya Sabeti <pouyasabeti.sgs@gmail.com>
2021-12-27 18:10:12 +0330

    added new

--- js/app.js
+++ js/app.js
@@ -8,3 +8,12 @@ function close_nav(){
     document.querySelector('.black-page').classList.remove("visibility-show");
     document.querySelector('.ss').classList.remove('visibility-hidden');
 }
+// var myvar = setInterval(time,10);
+//        function time(){
+        
+//         var b = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
+//         if(b < 867){
+//             document.getElementById("owl-2").classList = "ss";
+            
+//         }
+//        }
